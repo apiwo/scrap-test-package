b75bda73a0098379c42323acae1952a3ec22c520
apiwo <rayingeneral@gmail.com>
2026-04-05 15:10:05 -0400

    Add ASCII art of Tux the penguin in ascii.h

---
+++ ascii.h
@@ -0,0 +1,13 @@
+#ifndef ASCII_H
+#define ASCII_H
+
+const char* tux_ascii = 
+"    .--.\n"
+"   |o_o |\n"
+"   |:_/ |\n"
+"  //   \\ \\\n"
+" (|     | )\n"
+"/'\\_   _/`\\\n"
+"\\___)=(___/\n";
+
+#endif
